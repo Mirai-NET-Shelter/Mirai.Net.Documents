c6e82bae1b41f638a1de68c8d9c80a2cd6ad6900
AHpx <AHpx@yandex.com>
2022-02-20 23:54:03 +0800

    Updated scro[t

--- deploy.bat
+++ deploy.bat
@@ -1,9 +1,24 @@
 cd book
 
+@echo off
+setlocal
+for /f "skip=8 tokens=2,3,4,5,6,7,8 delims=: " %%D in ('robocopy /l * \ \ /ns /nc /ndl /nfl /np /njh /XF * /XD *') do (
+ set "dow=%%D"
+ set "month=%%E"
+ set "day=%%F"
+ set "HH=%%G"
+ set "MM=%%H"
+ set "SS=%%I"
+ set "year=%%J"
+)
+
+
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m '%year%-%month%-%day% %HH%:%MM%:%SS%'
 
 git push -f https://github.com/SinoAHpx/Mirai.Net.Documents.git master:new-gh-pages
 
 cd ..
+
+endlocal
