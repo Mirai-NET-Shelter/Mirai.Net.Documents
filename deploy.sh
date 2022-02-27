8577ada8e90e9802b71ffd3a9ae5043c9a2c3880
AHpx <AHpx@yandex.com>
2022-02-28 00:32:11 +0800

    improved time fetcher

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@ mdbook build
 
 cd book
 
-date=$(date '+%Y-%m-%d %H:%M:%S')
+date=$(date '+%Y-%m-%d-%H-%M-%S')
 
 git init
 git add -A
-git commit -m $(date '+%Y-%m-%d')
+git commit -m $date
 
 git push -f https://github.com/SinoAHpx/Mirai.Net.Documents.git master:new-gh-pages
 
